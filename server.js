4dbcebb39474dbddc95308c87fea81326e761420
Nita Roberts <nitaroberts05@gmail.com>
2022-04-25 15:46:39 -0700

    fixed error in index.js

--- server.js
+++ server.js
@@ -1,37 +1,17 @@
 const express = require("express");
-const path = require("path");
-const { clog } = require("./middleware/clog");
-const api = require("./routes/index.js");
-
-const PORT = process.env.port || 3001;
-
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
+
+app.use(express.json());
 
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-// GET Route for homepage
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-// GET Route for feedback page
-app.get("/feedback", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/pages/feedback.html"))
-);
-
-// Wildcard route to direct users to a 404 page
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/pages/404.html"))
-);
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => {
+  console.log(`API server is ready on port ${PORT}!`);
+});
